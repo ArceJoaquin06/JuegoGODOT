6034a586640d181af146ed1eef8d1cf2347d71c9
Joaquín <d47174994@alumnos.itr.edu.ar>
2025-06-26 19:47:15 -0300

    esta hecho el menu principal, falta hacer la interfaz de opciones

---
+++ jueguito/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://PrimerCaracter.tscn")
+
+
+func _on_opciones_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
