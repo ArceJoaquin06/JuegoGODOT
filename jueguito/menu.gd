e7536ef01920dd367925942374c13bba9509da1c
Joaquin Arce <d47174994@alumnos.itr.edu.ar>
2025-06-26 22:27:30 -0300

    no doy mas loco, otra vez

--- jueguito/menu.gd
+++ jueguito/menu.gd
@@ -7,7 +7,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_opciones_pressed() -> void:
-	get_tree().change_scene_to_file("res://opciones.tscn")
+	get_tree().change_scene_to_file("res://options.tscn")
 
 
 func _on_exit_pressed() -> void:
