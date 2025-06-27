8d433d2656d42d234661f6792d9b1c88ec4ed6de
ArceJoaquin06 <d47174994@alumnos.itr.edu.ar>
2025-06-27 15:27:27 -0300

    no hice casi nada, pero lo subo

--- jueguito/options.gd
+++ jueguito/options.gd
@@ -1,6 +1,7 @@
 extends Control
 
 
+@onready var brillo = get_node("$TabContainer/Pantalla/MarginContainer/GridContainer/HBoxContainer/HSlider")
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
