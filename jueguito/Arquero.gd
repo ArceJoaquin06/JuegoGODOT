8a9f9bce850ab10cfa72a50a4ee221e805bc12e4
unknown <vicente>
2025-07-23 13:31:53 -0300

    ahí invertí los controles de los players (Joaco)

--- jueguito/Arquero.gd
+++ jueguito/Arquero.gd
@@ -4,8 +4,6 @@ var escalera = false
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
-func _ready():
-	$AnimatedSprite2D.play("Idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -25,14 +23,30 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if Input.is_action_just_pressed("Dodge_Arquero") and is_on_floor() and direction != 0:
+		velocity.x = direction * SPEED * 2
+		$AnimatedSprite2D.play("Dodge")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	animations(direction)
 
 	if direction ==1:
 		$AnimatedSprite2D.flip_h =false
 	elif direction == -1:
 		$AnimatedSprite2D.flip_h =true
+		
+		
+		
+func animations(direction):
+	if is_on_floor():
+		if direction==0:
+			$AnimatedSprite2D.play("Idle")
+		else:
+			$AnimatedSprite2D.play("Run")
