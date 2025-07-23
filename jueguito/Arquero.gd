5ef03290b382489405600319d3acc5ce54604c8b
Joaquin Arce <d47174994@alumnos.itr.edu.ar>
2025-07-22 23:02:35 -0300

    le agrego los botones al arquero

--- jueguito/Arquero.gd
+++ jueguito/Arquero.gd
@@ -1,11 +1,31 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
 
-const SPEED_A = 70
-var velocity = Vector2.ZERO
+const SPEED = 200.0
+const JUMP_VELOCITY = -400.0
 
 func _ready():
-	velocity.x = -SPEED_A
-	play("Idle")
+	$AnimatedSprite2D.play("Idle")
 
-func _process(delta):
-	velocity.x = -SPEED_A
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Arriba_Arquero") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Izq_Arquero", "Der_Arquero")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	if direction ==1:
+		$AnimatedSprite2D.flip_h =false
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h =true
