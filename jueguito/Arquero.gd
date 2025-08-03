a7201125570e88afc6b85daf211ea42edb2c5c88
Joaquin <arcejoaquin06@gmail.com>
2025-08-03 17:02:32 -0300

    le puse el dash del arquero, solo falta su funcionabilidad

--- jueguito/Arquero.gd
+++ jueguito/Arquero.gd
@@ -9,6 +9,10 @@ const JUMP_VELOCITY = -400.0
 var nodoSalud2
 var esta_muerto = false
 
+var esquivando = false
+var tiempo_esquive = 0.3
+var tiempo_restante_esquive = 0.0
+
 var flecha = preload("res://flecha.tscn")
 
 func _ready():
@@ -20,50 +24,57 @@ func _physics_process(delta: float) -> void:
 	if esta_muerto:
 		return
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+
 	if escalera:
 		if Input.is_action_pressed("ui_up"):
 			velocity.y = -50
 		elif Input.is_action_pressed("ui_down"):
 			velocity.y = 50
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("ui_left", "ui_right")
-	
-	if Input.is_action_just_pressed("Dodge_Arquero") and is_on_floor() and direction != 0:
+
+	# Esquive
+	if Input.is_action_just_pressed("Dodge_Arquero") and is_on_floor() and direction != 0 and !esquivando:
+		esquivando = true
+		tiempo_restante_esquive = tiempo_esquive
 		velocity.x = direction * SPEED * 2
 		$AnimatedSprite2D.play("Dodge")
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
+	# Contador de esquive
+	if esquivando:
+		tiempo_restante_esquive -= delta
+		if tiempo_restante_esquive <= 0:
+			esquivando = false
+	else:
+		# Movimiento normal solo si no estás esquivando
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	animations(direction)
 
-	if direction ==1:
-		$AnimatedSprite2D.flip_h =false
+	move_and_slide()
+
+	# Flip sprite
+	if direction == 1:
+		$AnimatedSprite2D.flip_h = false
 	elif direction == -1:
-		$AnimatedSprite2D.flip_h =true
+		$AnimatedSprite2D.flip_h = true
 		
 		
 		
 func animations(direction):
-	if esta_muerto:
+	if esta_muerto or esquivando:
 		return
-		
+	
 	if is_on_floor():
-		if direction==0:
+		if direction == 0:
 			$AnimatedSprite2D.play("Idle")
 		else:
 			$AnimatedSprite2D.play("Run")
