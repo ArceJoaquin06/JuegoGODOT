6b65cea10e4707c9fed5bb0bf84910f6158f26f7
Joaquin <arcejoaquin06@gmail.com>
2025-08-08 06:28:01 -0300

    jhvjhvkghvkhh jkh

--- jueguito/main.gd
+++ jueguito/main.gd
@@ -1,8 +1,20 @@
 extends Node2D
 
 @onready var opciones = $Options
+@onready var juego = $Jugar
+@onready var menu_principal = $Menu
 
-func _on_ajustes_botón_pressed():
+func _ready():
+	juego.visible = false
+	opciones.visible = false
+
+func _on_play_pressed() -> void:
+	menu_principal.visible = false
+	juego.visible = true
+
+func _on_opciones_button_up() -> void:
 	opciones.visible = true
-	get_tree().paused = true
-	opciones.pause_mode = Node.PAUSE_MODE_PROCESS
+	opciones.pause_mode = 2 # ✅ "PROCESS"
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
--- jueguito/menu.gd
+++ jueguito/menu.gd
@@ -2,17 +2,17 @@ extends Control
 
 
 
-func _on_play_pressed() -> void:
-	$MenuPrincipal.visible = false
-	$Juego.visible = true
-	$UI.visible = true
-	$Juego.iniciar_juego()
+#func _on_play_pressed() -> void:
+#	$MenuPrincipal.visible = false
+#	$Juego.visible = true
+#	$UI.visible = true
+#	$Juego.iniciar_juego()
 
 
-func _on_opciones_pressed() -> void:
-	$Opciones.visible = true
-	$Opciones.pause_mode = Node.PAUSE_MODE_PROCESS
+#func _on_opciones_pressed() -> void:
+#	$Opciones.visible = true
+#	$Opciones.pause_mode = Node.PAUSE_MODE_PROCESS
 
 
-func _on_exit_pressed() -> void:
-	get_tree().quit()
+#func _on_exit_pressed() -> void:
+#	get_tree().quit()
